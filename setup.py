4d0739e305c504b53639468043e96533b35c5ff6
danielduberg <danielduberg@gmail.com>
2023-07-12 00:23:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\\n" + fh.read()
 
 setup(
-    name="ufo",
+    name="pyufo",
     version='{{VERSION_PLACEHOLDER}}',
     author="Daniel Duberg",
     author_email="danielduberg@gmail.com",
