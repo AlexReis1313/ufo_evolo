cb1d01ef16b107aa45ecfac15dfd15b316ea37d4
danielduberg <danielduberg@gmail.com>
2023-07-11 23:48:13 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+setup.py file
+"""from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
+
+setup(
+    name="ufo",
+    version='{{VERSION_PLACEHOLDER}}',
+    author="Daniel Duberg",
+    author_email="danielduberg@gmail.com",
+    description="UFO",
+    url = "https://github.com/UnknownFreeOccupied/ufo",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['pypi', 'cicd', 'python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
